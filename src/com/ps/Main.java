15ee6c612c8e4c10125923c76741f0f5bfb19789
Dimitrov2005 <s.dimitrov2005@gmail.com>
2021-01-30 13:27:02 +0200

    Implementation with no standard DS libraries
    Create simple map structure to hold key-value pairs and 
    update persistent storage to not use the standard data 
    structures.

--- src/com/ps/Main.java
+++ src/com/ps/Main.java
@@ -2,9 +2,10 @@ package com.ps;
 
 public abstract class Main {
 
-    private static PersistentStorage persistentStorage;
+    private static PersistentStorageNoStd persistentStorage;
 
     public static void main(String[] args) {
+       /* Test Persistent Storage with std libs
         persistentStorage = new PersistentStorage();
         persistentStorage.put("anotherString", 12368);
         persistentStorage.put("anotherString12", 16);
@@ -12,5 +13,42 @@ public abstract class Main {
         System.out.format("got value from db = %s \n ", persistentStorage.get("anotherString12"));
 
         persistentStorage.remove("anotherString");
+        */
+
+        /*  Test Persistent Storage without std libs
+         */
+        persistentStorage = new PersistentStorageNoStd(40, 100);
+
+        persistentStorage.put("anotherString", 12368);
+        persistentStorage.put("anotherString12", 16);
+        System.out.format("got value from db = %s \n ", persistentStorage.get("anotherString12"));
+        persistentStorage.remove("anotherString");
+
+
+
+        /* Test Map Structure
+        MapStruct ms = new MapStruct();
+        char [] key = new char [20];
+        key[0] = 'a';
+        key[1] = 's';
+        char [] value = new char [20];
+        value[0] = '1';
+        value[1] = '2';
+        char [] key1 = new char [20];
+        key1[0] = 'b';
+        key1[1] = 's';
+        char [] value1 = new char [20];
+        value1[0] = '2';
+        value1[1] = '2';
+        ms.put(key, value);
+        ms.put(key1, value1);
+        char[] keyRed = ms.getValue(1);
+       System.out.println(String.format("Got 1'st element of the storage key = %s \n", String.valueOf(keyRed)));
+       char [] valByKey = ms.getValueByKey(key);
+       System.out.format("Got the value %s \n", String.valueOf(valByKey));
+       ms.contains(key1);
+       ms.remove(key1);
+ */
+
     }
 }
