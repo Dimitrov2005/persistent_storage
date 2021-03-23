32ff8912fc3749c7082af9aaa005eb116f341282
unknown <s.dimitrov2005@gmail.com>
2021-03-23 17:10:56 +0200

    Move generated database files to specific directory. Fixtures to the persister class

--- src/main/java/com/impl/PersisterJavaSerialization.java
+++ src/main/java/com/impl/PersisterJavaSerialization.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.*;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,29 +15,47 @@ public class PersisterJavaSerialization extends PersisterBase{
     private String storageFileName;
     private Logger logger;
     private File psFile;
+    protected String storagePath;
+    protected File storageDir;
 
     public PersisterJavaSerialization(String fileName){
+
+        //Create storage directory
+        this.storagePath = Paths.get("").toAbsolutePath().toString() + "\\.persistentDB\\";
+        this.storageDir = new File(storagePath);
+        storageDir.mkdir();
+
         this.logger = LogManager.getLogger(PersisterJavaSerialization.class);
         this.storageFileName = fileName;
-        this.psFile = new File(fileName);
+        this.psFile = new File(storagePath + "\\" + storageFileName);
     }
 
     @Override
     public Map<String, Object> getEntriesFromLocalStorage() {
         Map<String,Object> deserializedMap = new HashMap<>();
+
         if(psFile.exists() && psFile.canRead()) {
+            ObjectInputStream in = null;
+            FileInputStream file = null;
             try {
-                FileInputStream file = new FileInputStream(psFile);
-                ObjectInputStream in = new ObjectInputStream(file);
+                file = new FileInputStream(psFile);
+                in = new ObjectInputStream(file);
 
                 deserializedMap = (Map<String, Object>) in.readObject();
                 logger.info("Retrieved local map from file " + storageFileName);
 
-                in.close();
-                file.close();
+            } catch (EOFException e) { //Todo : clean up this, this is thrown when new file is created since it is empty
+                logger.info("", e);
             } catch (IOException | ClassNotFoundException e) {
                 logger.error("", e);
-                throw new RuntimeException(e);
+                throw new RuntimeException();
+            } finally {
+                try {
+                    if (in != null) in.close();
+                    if (file != null) file.close();
+                } catch (IOException e) {
+                    logger.error("", e);
+                }
             }
         }
         return deserializedMap;
